16396f85d2f826878464b31738d0cd81df000689
gewure <jasperreichardt@gmail.com>
2015-12-03 10:15:31 +0100

    mavenbugfix, ImageVisualizer

--- imagevisualiser/src/main/java/bean/ImageVisualiser.java
+++ imagevisualiser/src/main/java/bean/ImageVisualiser.java
@@ -1,25 +1,67 @@
 package bean;
 
 import impl.ImageEvent;
+import interfaces.EventHandler;
 import interfaces.ImageListener;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by sereGkaluv on 23-Nov-15.
  */
-public class ImageVisualiser extends Canvas implements ImageListener {
-    private static final int DEFAULT_IMAGE_PLACEHOLDER_SIZE = 100;
-    private int _width = DEFAULT_IMAGE_PLACEHOLDER_SIZE;
-    private int _height = DEFAULT_IMAGE_PLACEHOLDER_SIZE;
+public class ImageVisualiser extends Canvas implements ImageListener, EventHandler<ImageListener, ImageEvent> {
 
-    private transient BufferedImage _bufferedImage;
+    /* constructors */
+    public ImageVisualiser(BufferedImage bufferedImage) {
+        _bufferedImage = bufferedImage;
+    }
 
     public ImageVisualiser() {
         setSize(_width, _height);
     }
 
+    private static final int DEFAULT_IMAGE_PLACEHOLDER_SIZE = 100;
+    private int _width = DEFAULT_IMAGE_PLACEHOLDER_SIZE;
+    private int _height = DEFAULT_IMAGE_PLACEHOLDER_SIZE;
+
+    private transient BufferedImage _bufferedImage;
+    private static final Set<ImageListener> IMAGE_LISTENER_REGISTRY = new HashSet<>();
+
+    /* ImageListener Overrides */
+    @Override
+    public void addImageListener(ImageListener listener) {
+        IMAGE_LISTENER_REGISTRY.add(listener);
+    }
+
+    @Override
+    public void removeImageListener(ImageListener listener) {
+        IMAGE_LISTENER_REGISTRY.remove(listener);
+    }
+
+    @Override
+    public void notifyAllListeners(ImageEvent imageEvent) {
+        IMAGE_LISTENER_REGISTRY.forEach(listener -> listener.onImageEvent(imageEvent));
+    }
+
+    /* Canvas Overrides */
+    @Override
+    public void paint(Graphics g) {
+        setSize(_width, _height);
+        g.drawImage(_bufferedImage, 0, 0, this);
+    }
+
+    /* EventListener override */
+    @Override
+    public void onImageEvent(ImageEvent imageEvent) {
+        _bufferedImage = imageEvent.getImage();
+        repaint();
+    }
+
+
+    /* getter / setter */
     public int getImageWidth() {
         return _width;
     }
@@ -36,15 +78,5 @@ public class ImageVisualiser extends Canvas implements ImageListener {
         _height = height;
     }
 
-    @Override
-    public void paint(Graphics g) {
-        setSize(_width, _height);
-        g.drawImage(_bufferedImage, 0, 0, this);
-    }
 
-    @Override
-    public void onImageEvent(ImageEvent imageEvent) {
-        _bufferedImage = imageEvent.getImage();
-        repaint();
-    }
 }
